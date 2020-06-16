0fa671a1557a7e28aa61e7cb870f3ee66a1ee0f7
Konstantin Lypskyi <konstantinlypskyi@gmail.com>
2020-06-16 12:50:47 +0300

    Updates

---
+++ precache-manifest.8d044e10802d1178112e028a945e4e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb2e9a7452df735ada581f900e91615",
+    "url": "/zoom-sample/index.html"
+  },
+  {
+    "revision": "f8f0ec0d7d597854c6f3",
+    "url": "/zoom-sample/static/css/main.45303ba3.chunk.css"
+  },
+  {
+    "revision": "3cfaca9d2e131a8f07b9",
+    "url": "/zoom-sample/static/js/2.10eff877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zoom-sample/static/js/2.10eff877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f0ec0d7d597854c6f3",
+    "url": "/zoom-sample/static/js/main.34467f0e.chunk.js"
+  },
+  {
+    "revision": "f141aeead4d95dae2753",
+    "url": "/zoom-sample/static/js/runtime-main.3392ca6c.js"
+  }
+]);
